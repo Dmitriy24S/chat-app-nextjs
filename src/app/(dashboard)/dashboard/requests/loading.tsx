cf1c286425e767a7c1a05dcfc8c26be2301605f8
Dmitriy24S <devilins24@gmail.com>
2023-06-04 20:51:27 +0300

    feat: add loading skeletons to dashboard pages
    * add loading skeletons to 'add friend', 'chat' and 'requests' pages

--- src/app/(dashboard)/dashboard/requests/loading.tsx
+++ src/app/(dashboard)/dashboard/requests/loading.tsx
@@ -1,14 +1,15 @@
 import Skeleton from 'react-loading-skeleton'
 import 'react-loading-skeleton/dist/skeleton.css'
 
-const LoadingAddSkeleton = () => {
+const LoadingRequestsSkeleton = () => {
   return (
     <div className='flex w-full flex-col gap-3'>
       <Skeleton className='mb-4' height={60} width={500} />
-      <Skeleton height={20} width={150} />
-      <Skeleton height={50} width={400} />
+      <Skeleton height={50} width={350} />
+      <Skeleton height={50} width={350} />
+      <Skeleton height={50} width={350} />
     </div>
   )
 }
 
-export default LoadingAddSkeleton
+export default LoadingRequestsSkeleton
